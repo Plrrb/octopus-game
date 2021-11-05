c415ba193bee9852a6c8c81d9ae2dbfa8b4e7865
plrrb <plrrbscript@gmail.com>
2021-11-05 14:18:30 -0700

    networked textures and animations

--- newocto.py
+++ newocto.py
@@ -3,6 +3,7 @@
 # OR:
 # keep it separate, stupid
 
+# A Base Class should only only share the similarities between the sub classes
 
 # functions should only do one thing
 #   classes should only be one thing
@@ -15,122 +16,6 @@ import sys
 GRAVITY = 9.8 / 20
 
 
-class Character_Texture_System:
-    def __init__(self, url):
-        self.textures_tuple = self.load_textures(url)
-        self.walk_textures = self.load_walk_textures(url)
-
-        self.texture = self.textures_tuple[0][0]
-
-    def set_texture_state(self, texture_num):
-        self.texture = self.textures_tuple[texture_num][self.direction]
-
-    def update_texture(self, on_ground):
-        self.update_direction()
-
-        # update 0 = idle, 1 = jump, 2 =fall
-        if self.change_y > 0:
-            self.set_texture_state(1)
-
-        elif self.change_y < 0:
-            self.set_texture_state(2)
-
-        elif 0.01 > self.change_x > -0.01 and on_ground:
-            self.set_texture_state(0)
-            self.reset_walk()
-
-        else:
-            self.update_walk()
-
-    def update_walk(self):
-        self.walk_index += 1
-
-        if self.walk_index > len(self.walk_textures) * self.animation_speed_reducer - 1:
-            self.reset_walk()
-
-        frame = self.walk_index // self.animation_speed_reducer
-
-        self.texture = self.walk_textures[frame][self.direction]
-
-    def update_direction(self):
-        if self.change_x == 0:
-            pass
-        elif self.change_x > 0:
-            self.direction = 0
-        else:
-            self.direction = 1
-
-    def reset_walk(self):
-        self.walk_index = 0
-
-    @staticmethod
-    def load_textures(url):
-        return (
-            (arcade.load_texture_pair(url + "idle.png")),
-            (arcade.load_texture_pair(url + "jump.png")),
-            (arcade.load_texture_pair(url + "fall.png")),
-        )
-
-    @staticmethod
-    def load_walk_textures(url):
-        return (
-            (arcade.load_texture_pair(url + "walk0.png")),
-            (arcade.load_texture_pair(url + "walk2.png")),
-            (arcade.load_texture_pair(url + "walk3.png")),
-            (arcade.load_texture_pair(url + "walk4.png")),
-            (arcade.load_texture_pair(url + "walk5.png")),
-            (arcade.load_texture_pair(url + "walk6.png")),
-            (arcade.load_texture_pair(url + "walk7.png")),
-        )
-
-
-class Controls:
-    def __init__(self):
-        self.inputs = {None: None}
-
-    def get(self, input_name):
-        return self.inputs.get(input_name, None)
-
-    def press(self, input_name, value):
-        self.inputs[input_name] = value
-
-
-class Player(arcade.Sprite, Character_Texture_System):
-    def __init__(self, url):
-        arcade.Sprite.__init__(self)
-        Character_Texture_System.__init__(self, url)
-
-        # left = 0, right = 1
-        self.direction = 0
-        self.max_speed = 3
-        self.animation_speed_reducer = 10
-        self.walk_index = 0
-        self.friction = 0.5
-        self.acceleration_reducer = 2
-        self.jump_power = 10
-
-        self.center_x = 500
-        self.center_y = 500
-
-        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
-
-    def jump(self):
-        self.change_y += self.jump_power
-        arcade.play_sound(self.jump_sound)
-
-    def movement_math(self):
-        return (abs(self.change_x) - self.max_speed) / self.acceleration_reducer
-
-    def move_left(self):
-        self.change_x += self.movement_math()
-
-    def move_right(self):
-        self.change_x -= self.movement_math()
-
-    def stop_moving(self):
-        self.change_x *= self.friction
-
-
 class Main_View(arcade.View):
     def __init__(self, character_url):
         super().__init__()
@@ -138,11 +23,8 @@ class Main_View(arcade.View):
         self.controls = Controls()
 
         self.make_walls()
-        self.player = Player(character_url)
+        self.player = Controllable_Player(character_url)
         self.make_physics_engine()
-
-        self.left_pressed_before_right = False
-        self.right_pressed_before_left = False
 
     def on_draw(self):
         arcade.start_render()
@@ -151,15 +33,17 @@ class Main_View(arcade.View):
         self.wall_list.draw()
 
     def on_update(self, delta_time):
-        self.cached_player_can_jump = self.physics_engine.can_jump()
-
+        self.physics_engine.update()
         self.update_player_contols()
 
         self.player.update()
-
         self.player.update_texture(self.cached_player_can_jump)
+        self.player_can_jump_was_cached = False
 
-        self.physics_engine.update()
+    def cached_player_can_jump(self):
+        if not self.player_can_jump_was_cached:
+            self.player_can_jump_was_cached = True
+            return self.physics_engine.can_jump()
 
     def update_player_contols(self):
         right = self.controls.get(arcade.key.D)
@@ -174,7 +58,7 @@ class Main_View(arcade.View):
         else:
             self.player.stop_moving()
 
-        if self.controls.get(arcade.key.SPACE) and self.cached_player_can_jump:
+        if self.controls.get(arcade.key.SPACE) and self.cached_player_can_jump():
             self.player.jump()
 
     def on_key_press(self, key, modifiers):
@@ -209,8 +93,8 @@ class Online_Game(Main_View):
         super().__init__(character_url)
         self.socket = socket
 
-        self.player2 = arcade.Sprite(
-            ":resources:images/animated_characters/robot/robot_idle.png"
+        self.player2 = Online_Player(
+            ":resources:images/animated_characters/robot/robot_"
         )
 
         t = threading.Thread(target=self.recv_other_players_pos, daemon=True)
@@ -226,7 +110,9 @@ class Online_Game(Main_View):
         self.player2.update()
 
     def send_our_pos(self):
-        self.socket.send(f"{self.player.center_x},{self.player.center_y}".encode())
+        self.socket.send(
+            f"{self.player.center_x},{self.player.center_y},{self.player.texture_number}".encode()
+        )
 
     def recv_other_players_pos(self):
         try:
@@ -235,7 +121,7 @@ class Online_Game(Main_View):
                 data = self.socket.recv(512)
                 data = data.decode()
                 data = eval(data)
-                self.player2.center_x, self.player2.center_y = data
+                self.player2.set_data(*data)
 
         except ConnectionResetError:
             print("Server Error!")
@@ -244,21 +130,144 @@ class Online_Game(Main_View):
             return
 
 
-def make_connection(ip, port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Controls:
+    def __init__(self):
+        self.inputs = {None: None}
 
-    # player2_ip = "162.196.90.150"
+    def get(self, input_name):
+        return self.inputs.get(input_name, None)
 
-    print("Trying to connect...")
-    client_socket.connect((ip, port))
-    print("Connected!")
+    def press(self, input_name, value):
+        self.inputs[input_name] = value
 
-    return client_socket
+
+class Base_Player(arcade.Sprite):
+    def __init__(self, character_url):
+        super().__init__()
+        self.texturess = self.make_players_textures(character_url)
+        self.change_texture(0, 0, 0)
+
+    def change_texture(self, is_walk, index, direction):
+        self.texture_number = is_walk, index, direction
+        self.texture = self.texturess[is_walk][index][direction]
+
+    def make_players_textures(self, url):
+        return (
+            (arcade.load_texture_pair(url + "idle.png")),
+            (arcade.load_texture_pair(url + "jump.png")),
+            (arcade.load_texture_pair(url + "fall.png")),
+        ), (
+            (arcade.load_texture_pair(url + "walk0.png")),
+            (arcade.load_texture_pair(url + "walk2.png")),
+            (arcade.load_texture_pair(url + "walk3.png")),
+            (arcade.load_texture_pair(url + "walk4.png")),
+            (arcade.load_texture_pair(url + "walk5.png")),
+            (arcade.load_texture_pair(url + "walk6.png")),
+            (arcade.load_texture_pair(url + "walk7.png")),
+        )
+
+
+class Controllable_Player(Base_Player):
+    def __init__(self, character_url):
+        super().__init__(character_url)
+
+        # left = 0, right = 1
+        self.direction = 0
+        self.max_speed = 3
+        self.animation_speed_reducer = 10
+        self.walk_index = 0
+        self.friction = 0.5
+        self.acceleration_reducer = 2
+        self.jump_power = 10
+
+        self.center_x = 500
+        self.center_y = 500
+
+        self.change_texture(0, 0, 0)
+        self.load_sounds()
+
+    def load_sounds(self):
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+
+    def update_texture(self, on_ground):
+        self.update_direction()
+
+        # update 0 = idle, 1 = jump, 2 =fall
+        if self.change_y > 0:
+            self.change_texture(0, 1, self.direction)
+
+        elif self.change_y < 0:
+            self.change_texture(0, 2, self.direction)
+
+        elif 0.01 > self.change_x > -0.01 and on_ground():
+            self.change_texture(0, 0, self.direction)
+            self.reset_walk()
+
+        else:
+            self.update_walk()
+
+    def update_walk(self):
+        self.walk_index += 1
+
+        if self.walk_index > len(self.texturess[1]) * self.animation_speed_reducer - 1:
+            self.reset_walk()
+
+        frame = self.walk_index // self.animation_speed_reducer
+
+        self.change_texture(1, frame, self.direction)
+
+    def update_direction(self):
+        if self.change_x == 0:
+            return
+        elif self.change_x > 0:
+            self.direction = 0
+        else:
+            self.direction = 1
+
+    def reset_walk(self):
+        self.walk_index = 0
+
+    def jump(self):
+        self.change_y += self.jump_power
+        arcade.play_sound(self.jump_sound)
+
+    def movement_math(self):
+        return (abs(self.change_x) - self.max_speed) / self.acceleration_reducer
+
+    def move_left(self):
+        self.change_x += self.movement_math()
+
+    def move_right(self):
+        self.change_x -= self.movement_math()
+
+    def stop_moving(self):
+        self.change_x *= self.friction
+
+
+class Online_Player(Base_Player):
+    def update(self):
+        super().update()
+        self.change_texture(*self.texture_number)
+
+    def set_data(self, x, y, texture_number):
+        self.center_x = x
+        self.center_y = y
+
+        self.texture_number = texture_number
 
 
 def main():
-    client_socket = make_connection(sys.argv[1], 5555)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # player2_ip = "162.196.90.150"
+    player2_ip = sys.argv[1]
+
+    # local ip for local connections
+    print(socket.gethostbyname(socket.gethostname()))
+
+    print("Trying to connect...")
+    client_socket.connect((player2_ip, 5555))
+    print("Connected!")
     window = arcade.Window(1000, 1000, "Octopus Game")
     screen = Online_Game(
         ":resources:images/animated_characters/male_adventurer/maleAdventurer_",
